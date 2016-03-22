a5bc7fd45ec42700f033a435a4e64740e355a682
Sagarika Kadambi <sskadamb@asu.edu>
2016-03-22 16:04:26 -0700

    building assignment 7

---
+++ point.py
@@ -0,0 +1,17 @@
+class Point(object):
+    def __init__(self,x,y,mark={}):
+        self.x = x
+        self.y = y
+        self.mark = mark
+
+    def patched_coincident(self,point2):
+        point1 = (self.x,self.y)
+
+        return check_coincident(point1,point2)
+
+    def patched_shift(self,x_shift,y_shift):
+        point = (self.x,self.y)
+        self.x,self.y = shift_point(point,x_shift,y_shift)
+
+
+from utils import check_coincident,shift_point
